6815d15834cbcb5fadc7e63253bf19ded2b64f16
Paula <herrra1402@gmail.com>
2015-11-19 22:32:47 +0100

    zadanie4

---
+++ app/app.js
@@ -0,0 +1,156 @@
+/*~~~~~~~~~~~PODSTAWY PROGRAMOWANIA~~~~~~~~~~~~*/
+
+/* if (day < 19) {
+	console.log(day + " jest mniejszy"); 
+} else if (day > 19) {
+	console.log(day + "jest większy");
+} else if (day = 19) {
+	console.log(day + 'jest dzisiaj')
+}*/
+/*~~~~~~~~~~~ZADANIE 4~~~~~~~~~~~~*/
+
+/*
+var number1 = 1000;
+var number2 = 3000; 
+var number3 = 1500;
+
+
+
+if (number1 > number2) { 
+	if (number1 > number3) {
+		console.log(number1)
+	};
+}
+
+if (number2 > number1) {
+	if (number2 > number3) {
+		console.log(number2)
+	};
+}
+
+if (number3 > number1) {
+	if (number3 > number2) {
+		console.log(number3)
+	};
+}
+*/
+
+/*
+var ocena = 150;
+
+if (ocena < 39) {
+	console.log("ocena niedostateczna");
+} 
+else if (ocena < 54) {
+	console.log("ocena dopuszczająca");
+} 
+else if (ocena < 69) {
+	console.log("ocena dostateczna");
+} 
+else if (ocena < 84) {
+	console.log("ocena dobra");
+} 
+else if (ocena < 98) {
+	console.log("ocena bardzo dobra");
+}
+else if (ocena <100) {
+	console.log("ocena celująca");
+}
+else if (ocena >100) {
+	console.log("error");
+}
+*/
+/*
+var i = 15;
+ 
+while( i < 15 ) {
+	console.log("Wartość i= " +1);
+	i=1+1;
+}
+console.log("Koniec");
+
+for (var i=0; i<10; i=i+1){
+	console.log(i);
+}
+*/
+
+/*
+var l = Math.floor(Math.random() * (20));
+var counter = 0;
+while (l != 10) {
+	console.log("Zła liczba: " + l);
+	l = Math.floor(Math.random() * (20));
+	counter = counter + 1;
+}
+
+console.log("dobra liczba" +1);
+console.log(counter);
+*/
+
+/*
+var m = 1;
+var silnia2 = 1;
+
+
+while (m <= 5) {
+	console.log(m);
+	silnia2 = silnia2 * m;
+	console.log(silnia2 + "*" +m);
+	m=m+1
+}
+
+
+console.log(silnia2);
+*/
+
+/*
+var suma = 0;
+var x = 5;
+var y =10;
+
+while (x <= y) {
+	suma = suma + x;
+	x = x + 1;
+}
+
+console.log(suma)
+*/
+
+var liczba1 = 459;
+var liczba2 = 45;
+var liczba3 = 2367;
+
+if(liczba1 > liczba2) {
+	else if(liczba2 > liczba3) {
+		else if (liczba2 > liczba3) {
+			else(liczba3 > liczba2)
+				console.log ( +"jestst najwyższą liczbą")
+		};
+	};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
