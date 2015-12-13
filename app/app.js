9e48c44750d1253940811860d61ebbdddc705f1c
Paula <herrra1402@gmail.com>
2015-12-14 00:55:53 +0100

    update

--- app/app.js
+++ app/app.js
@@ -44,7 +44,6 @@ $(document).ready(function(){
 
 
 /* SCROLL TOP BUTTON */
-	
 	$(window).on('scroll', function(){
 		if ($(this).scrollTop() > 200) {
 			$('.scrollToTop').fadeIn();
@@ -60,41 +59,64 @@ $(document).ready(function(){
 	});
 
 
+/* SCROLL DOWN BUTTON */
+
+
+
+
+/* REVIEVS */
+	$(window).on('scroll', doParallax);
+
+		
+		$('.dots-nav a').on('click', function() {
+			$('.dots-nav a').removeClass('active');
+			$(this).addClass('active');
+			var id = $(this).attr('data-slide');
+			slideChange(id);
+			return false;
+		});
+
+		function slideChange(id) {
+			$('.review-item').fadeOut(300);
+			$('.review-item#' + id).fadeIn(300);
+		}
+
+
+
 /* PARALLAX */
-
-var images = [].slice.call(document.querySelectorAll('.js-parallax-bg'));
-function getViewportHeight() {
-    var a = document.documentElement.clientHeight, b = window.innerHeight;
-    return a < b ? b : a;
-}
-    function getViewportScroll() {
-    if(typeof window.scrollY != 'undefined') {
-        return window.scrollY;
-    }
-    if(typeof pageYOffset != 'undefined') {
-        return pageYOffset;
-    }
-    var doc = document.documentElement;
-    doc = doc.clientHeight ? doc : document.body;
-    return doc.scrollTop;
+	var images = [].slice.call(document.querySelectorAll('.js-parallax-bg'));
+	function getViewportHeight() {
+	    var a = document.documentElement.clientHeight, b = window.innerHeight;
+	    return a < b ? b : a;
 	}
+	    function getViewportScroll() {
+	    if(typeof window.scrollY != 'undefined') {
+	        return window.scrollY;
+	    }
+	    if(typeof pageYOffset != 'undefined') {
+	        return pageYOffset;
+	    }
+	    var doc = document.documentElement;
+	    doc = doc.clientHeight ? doc : document.body;
+	    return doc.scrollTop;
+		}
 
-	function doParallax() {
-    var el, elOffset, elHeight,
-        offset = getViewportScroll(),
-        vHeight = getViewportHeight();
- 
-    for(var i in images) {
-        el = images[i];
-        elOffset = el.offsetTop;
-        elHeight = el.offsetHeight;
- 
-        if((elOffset > offset + vHeight) || (elOffset + elHeight < offset)) { continue; }
- 
-        el.style.backgroundPosition = '50% '+Math.round((elOffset - offset)*3/8)+'px';
-    }
+		function doParallax() {
+	    var el, elOffset, elHeight,
+	        offset = getViewportScroll(),
+	        vHeight = getViewportHeight();
+	 
+	    for(var i in images) {
+	        el = images[i];
+	        elOffset = el.offsetTop;
+	        elHeight = el.offsetHeight;
+	 
+	        if((elOffset > offset + vHeight) || (elOffset + elHeight < offset)) { continue; }
+	 
+	        el.style.backgroundPosition = '50% '+Math.round((elOffset - offset)*3/8)+'px';
+	    }
 }
-Gator(window).on('scroll', doParallax);
+
 
 
 });
